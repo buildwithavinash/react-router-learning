68ada9c8bd4fbcc083391a0b3ecb2354ed9c0c25
Avinash <buildwithavinash@users.noreply.github.com>
2026-04-15 00:52:21 +0530

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import AboutPage from "./pages/AboutPage"
+import Courses from "./pages/Courses"
+import HomePage from "./pages/HomePage"
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/course" element={<Courses />}/>
+        <Route path="/about" element={<AboutPage />}/>
+      </Routes>
+    </>
   )
 }
 
